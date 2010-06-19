838024e3763cb43f722b70bfb8bbeb31e82cacc5
Marcus Ortiz <mportiz08@gmail.com>
2010-06-19 15:33:51 -0700

    added Sprite class

---
+++ src/com/marcusortiz/burnination/Sprite.java
@@ -0,0 +1,128 @@
+package com.marcusortiz.burnination;
+
+import android.graphics.Bitmap;
+
+public class Sprite
+{
+  private Bitmap bitmap;
+  private Coordinates location;
+  private Speed speed;
+
+  public Sprite(Bitmap bitmap)
+  {
+    this.bitmap = bitmap;
+    this.location = new Coordinates();
+    this.speed = new Speed();
+  }
+
+  public Bitmap getGraphic()
+  {
+    return this.bitmap;
+  }
+
+  public Coordinates getLocation()
+  {
+    return this.location;
+  }
+
+  public Speed getSpeed()
+  {
+    return this.speed;
+  }
+
+  public class Coordinates
+  {
+    private int x = 0;
+    private int y = 0;
+
+    public int getX()
+    {
+      return x + bitmap.getWidth() / 2;
+    }
+
+    public void setX(int x)
+    {
+      this.x = x - bitmap.getWidth() / 2;
+    }
+
+    public int getY()
+    {
+      return y + bitmap.getHeight() / 2;
+    }
+
+    public void setY(int y)
+    {
+      this.y = y - bitmap.getHeight() / 2;
+    }
+  }
+
+  public class Speed
+  {
+    public static final int X_DIRECTION_RIGHT = 1;
+    public static final int X_DIRECTION_LEFT = -1;
+    public static final int Y_DIRECTION_DOWN = 1;
+    public static final int Y_DIRECTION_UP = -1;
+
+    private int x = 1;
+    private int y = 1;
+    private int xDir = X_DIRECTION_RIGHT;
+    private int yDir = Y_DIRECTION_DOWN;
+
+    public int getxDir()
+    {
+      return xDir;
+    }
+
+    public void setxDir(int xDir)
+    {
+      this.xDir = xDir;
+    }
+
+    public void toggleXDir()
+    {
+      xDir = (xDir == X_DIRECTION_RIGHT) ? X_DIRECTION_LEFT : X_DIRECTION_RIGHT;
+    }
+
+    public int getyDir()
+    {
+      return yDir;
+    }
+
+    public void setyDir(int yDir)
+    {
+      this.yDir = yDir;
+    }
+
+    public void toggleYDir()
+    {
+      yDir = (yDir == Y_DIRECTION_DOWN) ? Y_DIRECTION_UP : Y_DIRECTION_DOWN;
+    }
+
+    public int getX()
+    {
+      return x;
+    }
+
+    public void setX(int x)
+    {
+      this.x = x;
+    }
+
+    public int getY()
+    {
+      return y;
+    }
+
+    public void setY(int y)
+    {
+      this.y = y;
+    }
+
+    @Override
+    public String toString()
+    {
+      String xDir = (this.xDir == X_DIRECTION_RIGHT) ? "right" : "left";
+      return "Speed: x: " + this.x + " | y: " + this.y + " | xDirection: " + xDir;
+    }
+  }
+}
