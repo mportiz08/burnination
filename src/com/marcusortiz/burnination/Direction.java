5f29a2f85aaebf595460b71cd54d6f22c8edc58f
Marcus Ortiz <mportiz08@gmail.com>
2010-07-01 16:25:33 -0700

    working on a method to find the direction, given a line

--- src/com/marcusortiz/burnination/Direction.java
+++ src/com/marcusortiz/burnination/Direction.java
@@ -9,6 +9,15 @@ public class Direction
   public static final int DOWN = 1;
   public static final int UP = -1;
   
+  public int xDir;
+  public int yDir;
+  
+  public Direction(int xDir, int yDir)
+  {
+    this.xDir = xDir;
+    this.yDir = yDir;
+  }
+  
   public static int randomX()
   {
     int[] directions = {RIGHT, LEFT};
--- src/com/marcusortiz/burnination/Geometry.java
+++ src/com/marcusortiz/burnination/Geometry.java
@@ -14,4 +14,64 @@ public class Geometry
   {
     return Math.sqrt( (b.y - a.y)*(b.y - a.y) + (b.x - a.x)*(b.x - a.x) );
   }
+  
+  public static Direction findDir(Point a, Point b)
+  {
+    int xDir = 0;
+    int yDir = 0;
+    int xlen = b.x - a.x;
+    int ylen = b.y - a.y;
+    double halfPi = Math.PI / 2;
+    double twoPi = 2 * Math.PI;
+    double angle = Math.atan2(ylen, xlen);
+    
+    // my old friend, the unit circle
+    if(angle == 0 || angle == twoPi)
+    {
+      xDir = Direction.RIGHT;
+      yDir = 0;
+    }
+    else if(angle > 0 && angle < halfPi)
+    {
+      xDir = Direction.RIGHT;
+      yDir = Direction.UP;
+    }
+    else if(angle == halfPi)
+    {
+      xDir = 0;
+      yDir = Direction.UP;
+    }
+    else if(angle > halfPi && angle < Math.PI)
+    {
+      xDir = Direction.LEFT;
+      yDir = Direction.UP;
+    }
+    else if(angle == Math.PI)
+    {
+      xDir = Direction.LEFT;
+      yDir = 0;
+    }
+    else if(angle < 0 && angle > (-(halfPi)))
+    {
+      xDir = Direction.RIGHT;
+      yDir = Direction.DOWN;
+    }
+    else if(angle == -(halfPi))
+    {
+      xDir = 0;
+      yDir = Direction.DOWN;
+    }
+    else if(angle < (-(halfPi)) && angle > (-(Math.PI)))
+    {
+      xDir = Direction.LEFT;
+      yDir = Direction.DOWN;
+    }
+    else if(angle == -(Math.PI))
+    {
+      xDir = Direction.LEFT;
+      yDir = 0;
+    }
+    
+    return new Direction(xDir, yDir);
+  }
 }
